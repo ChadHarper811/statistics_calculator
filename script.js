86882a2e0982af2723d1cd163e5dc4fa316b5708
Chad Harper <chadharper811@gmail.com>
2024-06-29 17:42:26 -0500

    added starting constants to function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 const calculate = () => {
     const value = document.querySelector(".value").value;
+    const array = value.split(/,\s*/g);
+    const numbers = array.map(el => Number(el))
+    const filtered = numbers.filter()
 }
