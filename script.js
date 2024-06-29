8b571b17a3154d9239aaff156b5b625fb9c86254
Chad Harper <chadharper811@gmail.com>
2024-06-29 17:50:50 -0500

    finished mean func

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const calculate = () => {
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
 }
+
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0)/array.length;
