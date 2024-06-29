a48ba8a25b24df596f299d2f3149e76c2e10669e
Chad Harper <chadharper811@gmail.com>
2024-06-29 18:03:39 -0500

    made sure page doesn't reload and added mean to page

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
+const getMean = (array) => array.reduce((acc, el) => acc + el, 0)/array.length;
+
+const getMedian = (array) => {
+    
+}
+
 const calculate = () => {
     const value = document.querySelector(".value").value;
     const array = value.split(/,\s*/g);
     const numbers = array.map(el => Number(el)).filter(el => !isNaN(el));
+
+    const mean = getMean(numbers);
+    document.querySelector("#mean").textContent = mean;
 }
 
-const getMean = (array) => array.reduce((acc, el) => acc + el, 0)/array.length;
