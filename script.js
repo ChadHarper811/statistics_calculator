e11bcd0af3db17621bce03f61cdade6aaa96b8af
Chad Harper <chadharper811@gmail.com>
2024-07-06 09:19:14 -0500

    median function working

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 const getMean = (array) => array.reduce((acc, el) => acc + el, 0)/array.length;
 
 const getMedian = (array) => {
-    const sorted = array.sort();
+    const sorted = array.sort((a,b) => a-b);
+    if (sorted.length % 2 === 1) {
+        return sorted[Math.floor(sorted.length / 2)];
+    } else if (sorted.length % 2 === 0) {
+        return getMean([sorted[sorted.length / 2], sorted[(sorted.length / 2) - 1]])
+    }
 }
 
 const calculate = () => {
